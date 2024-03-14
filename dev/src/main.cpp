e5b2d41671c05e874c17b1f08da97c3d5bb53a02
kyyneogs <oks990304@gmail.com>
2024-03-14 22:15:30 +0900

    feat : Add connection close sync with server

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -11,14 +11,17 @@ int main(){
     int port;
     std::cout << "set Server Port : ";
     std::cin >> port;
+    if (port == 0)
+        exit(1);
     client.setPort(port);
     client.sockInit();
+    std::cin.ignore(1);
 
     while(1){
         std::string message;
         std::cout << "Enter Message : ";
         getline(std::cin, message);
-        if (message=="q")
+        if (message == "")
             break;
         
         client.sendMessage(message);
